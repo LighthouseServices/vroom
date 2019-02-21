cb1b72f5b6af1a1b8094d3e164a4d2f1acc18dd9
Ike <ike@litehouse.nyc>
2019-02-21 09:49:12 -0500

    Final version before interview

--- home_page.py
+++ home_page.py
@@ -1,3 +1,53 @@
 #!/usr/bin/env python
+import Driver
+from selenium.webdriver.common.by import By
+import time
 
-class HomePage(
+class HomePage:
+    def __init__(self):
+        self.title = "Vroom: Buy, Sell or Trade-In Used Vehicles Online"
+        self.go_to_url()
+
+    @staticmethod
+    def go_to_url():
+        Driver.Instance.get("https://vroom.com")
+
+    # Selenium wrappers for all fields on the HomePage
+    @staticmethod
+    def get_search_textfield():
+        return Driver.Instance.find_element(By.CSS_SELECTOR, "#hero-search-input")
+
+    @staticmethod
+    def get_search_button():
+        return Driver.Instance.find_element(By.CSS_SELECTOR, "#marquee > div > form > span > button")
+
+    # Selenium methods for setting fields
+    def set_search_textfield(self, search_str):
+        self.get_search_textfield().send_keys(search_str)
+
+    def click_search_button(self):
+        return self.get_search_button().click() 
+
+    # Business Function(s)
+    def search(self, search_str):
+        self.set_search_textfield(search_str)
+        self.click_search_button()
+
+        # TODO: waitUntil page is rendered
+        time.sleep(1)
+
+        return SearchResultsPage()
+
+class SearchResultsPage:
+    def __init__(self):
+        self.title = "Buy Low-Mileage Used Cars & Trucks Online - Vroom"
+
+    @staticmethod
+    def get_cars_list():
+        return Driver.Instance.find_element(By.XPATH, "//*[@id='cars-section']/ul")
+
+    def get_all_cars(self):
+        car_list_section = self.get_cars_list()
+        cars_list = car_list_section.find_elements(By.CSS_SELECTOR, "a[href*=inventory]")
+
+        return cars_list
--- vroom.py
+++ vroom.py
@@ -4,25 +4,33 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import Driver
+from home_page import HomePage
+
+import requests
 
 
 class Testing(unittest.TestCase):
     def setUp(self):
         Driver.Initialize()
-        Driver.Instance.get("http://www.python.org")
 
-    def test_vroom_search(self):
-        assert "Python" in Driver.Instance.title
-        elem = Driver.Instance.find_element_by_name("q")
-        elem.clear()
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in Driver.Instance.page_source
+    def test_vroom_homepage(self):
+        HomePage.go_to_url()
+        self.assertEqual("Vroom: Buy, Sell or Trade-In Used Vehicles Online", Driver.Instance.title)
 
-    def test_boolean(self):
-        a = True
-        b = True
-        self.assertEqual(a, b)
+    def test_vroom_homepage_search(self):
+        search_results_page = HomePage().search("bmw")
+        self.assertEqual(search_results_page.title, Driver.Instance.title)
+
+        cars_list = search_results_page.get_all_cars()
+        for car in cars_list:
+            print("car={}\n".format(car.text))
+
+        self.assertEqual(20, len(cars_list))
+
+    def test_api_request(self):
+        r = requests.get(url='https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+        print("status={}".format(r.status_code))
+        print("response={}".format(r.json()))
 
     def tearDown(self):
         Driver.Instance.close()
