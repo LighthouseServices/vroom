99d2c517b3a2d0e3639fef85affd4892cecb51f7
Ike <ike@litehouse.nyc>
2019-03-10 00:30:53 -0500

    Added utils for generating names, email and phone numbers in a common format by default.

--- Utils.py
+++ Utils.py
@@ -19,3 +19,24 @@ from random import choice
 from string import ascii_uppercase
 def generate_str(size=4):
     return ''.join(choice(ascii_uppercase) for i in list(range(size)))
+
+def generate_name(name, unique_id=None, use_name_only=False):
+    if use_name_only:
+        return name
+
+    if unique_id is None:
+        unique_id = generate_str()
+
+    return "{}-AUTO-{}".format(name, unique_id)
+
+def generate_email(address, host="test.com", unique_id=None, use_address_only=False):
+    if use_address_only:
+        return address
+
+    if unique_id is None:
+        unique_id = generate_str()
+
+    return "{}+auto-{}@{}".format(address, unique_id, host)
+
+def generate_phone_number(area_code="555", prefix="111", number="2222"):
+    return "{}{}{}".format(area_code, prefix, number)
